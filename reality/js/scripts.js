1d43095314f0e3d68f11b1d1182a1754cd4971b6
eduardopcjunior <eduardojunior1402@hotmail.com>
2022-07-25 17:57:49 -0300

    Ajuste

--- reality/js/scripts.js
+++ reality/js/scripts.js
@@ -1,90 +1,34 @@
-/* JS INICIAL PARA CEP/ENDEREÇO */
-const formulario = document.querySelector("form");
-const inputCep = formulario.querySelector("#cep");
-const inputTelefone = formulario.querySelector("#telefone");
-const inputEndereco = formulario.querySelector("#endereco");
-const inputBairro = formulario.querySelector("#bairro");
-const inputCidade = formulario.querySelector("#cidade");
-const inputEstado = formulario.querySelector("#estado");
-const bStatus = formulario.querySelector("#status");
-const botaoLocalizar = formulario.querySelector("#localizar-cep");
-const inputCelular = formulario.querySelector("#celular");
-
-botaoLocalizar.addEventListener("click", function (event){
-    event.preventDefault();
-    //Entrar no site viacep.com.br
-
-    /* Pegar o CEP digitado */
-    let cep = inputCep.value;
-    let url = `https://viacep.com.br/ws/${cep}/json/`;
-    console.log(url);
-
-    /* Ajax: comunicação assíncrona (os processos ocorrem paralelamente) com o Viacep usando a função chamada fetch */
-
-    // 1) Fazer a conexão com a API (ou acessar)
-    fetch(url)
-
-        //2) Então, recupere a resposta de acesso no formato jason
-        .then(resposta => resposta.json()) 
-
-            //3) Então, mostre os dados
-            .then(dados => {
-                if( "erro" in dados ) {
-                bStatus.innerHTML = "CEP não encontrado!"
-                inputCep.focus();
-            } else {
-                bStatus.innerHTML = "CEP encontrado!";
-                inputEndereco.value = dados.logradouro;
-                inputBairro.value = dados.bairro;
-                inputCidade.value = dados.localidade;
-                inputEstado.value = dados.uf;
-            }
-
-                
-            });
-});
-
-
-/*  estamos utilizando a LIB VanillaMasker - https://github.com/vanilla-masker/vanilla-masker  - para melhorar nossa resposta a função de busca de CEP*/
-
-VMasker(inputCep).maskPattern("99999-999");
-VMasker(inputTelefone).maskPattern("(99) 9999-9999");
-VMasker(inputCelular).maskPattern("(99) 9-9999-9999");
-
-
-//Programação do contador de caracteres do campo mensagem
+/* Programação do contador de caracteres
+do campo mensagem */
 const spanMaximo = formulario.querySelector("#maximo");
 const bCaracteres = formulario.querySelector("#caracteres");
 const textMensagem = formulario.querySelector("#mensagem");
 
-/* Objetivo da variavel é determinar a quantidade max de caracteres do campo "mensagem" */
+// Determinar a quantidade máxima de caracteres
 let quantidade = 100;
 
-//Evento para detectar a digitação (entrada) no campo
-textMensagem.addEventListener("input", function (){
-   
-    //capturando o que foi digitado
+// Evento para detectar a digitação (entrada) no campo
+textMensagem.addEventListener("input", function(){
+    
+    // Capturando o que for digitado
     let conteudo = textMensagem.value;
 
-    //contagem regressiva
+    // Criando uma contagem regressiva
     let contagem = quantidade - conteudo.length;
 
-    //Add a contagem ao elemento HTML
+    // Adicionando a contagem ao elemento HTML
     bCaracteres.textContent = contagem;
-    
-    if (contagem == 0) {
-        bCaracteres.style.color = "red"; 
-        textMensagem.style.boxShadow = "red 0 0 10px"
-    } else {
-        bCaracteres.style.color = "black"; 
-        textMensagem.style.boxShadow = "black 0 0 10px"
 
-        
+    if (contagem == 0) {
+        bCaracteres.style.color = "red";
+        textMensagem.style.boxShadow = "red 0 0 10px";
+    } else {
+        bCaracteres.style.color = "black";
+        textMensagem.style.boxShadow = "black 0 0 10px";
     }
 });
 
-//Programação de envio do formulário copiada do Formspree:
-
+/* Programação do Formspree */
 var form = document.getElementById("my-form");
     
     async function handleSubmit(event) {
@@ -99,19 +43,19 @@ var form = document.getElementById("my-form");
         }
       }).then(response => {
         if (response.ok) {
-          status.innerHTML = "Mensagem enviada. Em breve retornaremos o contato!";
+          status.innerHTML = "Obrigado por enviar!";
           form.reset()
         } else {
           response.json().then(data => {
             if (Object.hasOwn(data, 'errors')) {
               status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
             } else {
-              status.innerHTML = "Oops! Deu ruim. Tente novamente em alguns minutos"
+              status.innerHTML = "Oops! Deu ruim! Tente novamente mais tarde"
             }
           })
         }
       }).catch(error => {
-        status.innerHTML = "Oops! Deu ruim. Tente novamente em alguns minutos"
+        status.innerHTML = "Oops! There was a problem submitting your form"
       });
     }
     form.addEventListener("submit", handleSubmit)
